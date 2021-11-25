d9c690fe7c7f0878c95c1f7e631a6e309a40b429
Ferdinando Letizia <ferdinando.letizia@outlook.it>
2021-11-25 14:44:37 +0100

    bonus img

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = new Vue({
     el: '#root',
     data: {
-        title: 'Hello World!'
+        title: 'Hello World!',
+        miaImmagine: 'https://picsum.photos/800/400',
     }
 });
